51b4ce08478e4df75722e3bc15972af964d942d5
Klein Lavina <160082336+KleinLavina@users.noreply.github.com>
2024-08-10 19:28:42 +0800

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,108 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Wall Clock 12 Inch Silent ',
+        image: '1.jpg',
+        price: 1995
+    },
+    {
+        id: 2,
+        name: 'Bernhard Products Black Wall Clock',
+        image: '2.jpg',
+        price: 1250
+    },
+    {
+        id: 3,
+        name: 'Westclox Traditional Large Wall Clock',
+        image: '3.jpg',
+        price: 1995
+    },
+    {
+        id: 4,
+        name: ' Fasmov 12 Vintage Bronze Round Wall Clock',
+        image: '4.jpg',
+        price: 1340
+    },
+    {
+        id: 5,
+        name: 'Mosewa Wall Clock, Battery Operated Silent Non-Ticking ',
+        image: '5.jpg',
+        price: 2000
+    },
+    {
+        id: 6,
+        name: 'CHYLIN Wall Clock Silent',
+        image: '6.jpg',
+        price: 1889
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="images/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">+ CART</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="images/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
