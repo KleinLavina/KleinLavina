d1eb1232467e12ec60bd1b2431af40669b3c97c1
Klein Lavina <160082336+KleinLavina@users.noreply.github.com>
2024-09-29 11:48:52 +0800

    Add files via upload

---
+++ KLEIN PHP/index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML AND PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <?php include'data.php'?>
+
+</head>
+<body>
+    <div class="header">
+        <?php include "header.php"; ?>
+    </div>
+    <div class="nav-bar">
+        <ul>
+            <li><a href='?page=homepage'>HOME</a></li>
+            <li><a href='?page=page1'>FOUR-COLUMN-ITEMS</a></li>
+            <li><a href='?page=page2'>TWO-COLUMN-ITEMS</a></li>
+            <li><a href='?page=login'>LOG IN</a></li>
+        </ul>
+    </div>
+
+<div class="all-content">
+<?php 
+	if (isset($_GET['page'])) {
+		$pg = $_GET['page'];
+			if ($pg == "page1") {
+				include "page1.php";
+				}
+				elseif ($pg == "page2") {
+				include "page2.php";
+				}
+				elseif ($pg == "login") {
+				include "login.php";
+				}
+				else {
+				include "homepage.php";
+					}
+		} else { 
+			include "homepage.php";
+		}	
+			?>
+
+</div>
+
+</body>
+</html>
---
+++ KLEIN PHP/login.php
@@ -0,0 +1,13 @@
+<div align="center" style="margin-top:50px;">
+    <div style="width:300px; border: solid 1px #333333; " align="left">
+     <div style="background-color:#333333; color:#FFFFFF; padding:3px;text-align:center;"><b>Login</b></div>
+     <div style="margin:30px">
+      <form>
+        <label>UserName  :</label><input type="text" name="username" class="box"/><br /><br />
+        <label>Password  :</label><input type="password" name="password" class="box" /><br/><br />
+        <input type="submit" value=" Submit "/><br />
+      </form>
+      <div style="font-size:11px; color:#cc0000; margin-top:10px"><?php echo "Error message here..."; ?></div>
+     </div>
+    </div>
+   </div>
