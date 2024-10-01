395785a931f9ebf43d733fd0e3dcfcb8c8d06dc1
Klein Lavina <160082336+KleinLavina@users.noreply.github.com>
2024-10-01 11:02:32 +0800

    Delete PHPKlein directory

--- PHPKlein/a/matalom.php
+++
@@ -1,7 +0,0 @@
-<?php 
-echo"<html>
-<h1>Matalom</h1>
-<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMTyNInGXIU9NzXgWPklB7NekL5w8zWfrhqA&s'>
-<p style='inline-block;float:bottom'>Matalom is a south western coastal town in the Province of Leyte and is classified as a third class Municipality. It has 30 barangays and includes the scenic islet of Canigao.</p>
-</html>
-"?>
--- PHPKlein/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo"
-<html>
-<h1>Marvelous Adventures and Insights into the World Starts With M</h1>
-<h3><b>BY: KLEIN F. LAVINA BSIT 3-T</b></h3>
-</html>"
-?>
