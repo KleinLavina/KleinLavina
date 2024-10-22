7c274080c885b0aa73a13fd6d03ee439a20c827e
Klein Lavina <160082336+KleinLavina@users.noreply.github.com>
2024-10-22 19:40:27 +0800

    Delete PHPKleinNew directory

--- PHPKleinNew/about.php
+++
@@ -1,6 +0,0 @@
-<section id="about">
-        <h2>About Us</h2>
-        <img src="images/logo.jpg" style="height:200px;width:250px;margin:auto;border-radius:20px;">
-        <p>Nyxify Collection is dedicated to providing the highest quality clothing with a focus on style, comfort, and individuality. Our mission is to help you express your unique style through our diverse collection.</p>
-        <p>We are passionate about fashion and committed to customer satisfaction.</p>
-    </section>
